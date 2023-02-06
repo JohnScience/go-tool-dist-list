32746fcd9a3b35238b12367e819159e021a58b3e
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-02-05 20:57:43 -0700

    Added support for efficient .contains()

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{ops::Deref, process::Command};
 
 pub struct Targets(Vec<u8>);
 
@@ -7,6 +7,13 @@ impl Targets {
         std::str::from_utf8(&self.0)
             .map(str::lines)
             .map(TargetsIter)
+    }
+
+    pub fn collect_strs(&self) -> Result<CollectedTargetsStrs, std::str::Utf8Error> {
+        std::str::from_utf8(&self.0)
+            .map(str::lines)
+            .map(|targets| targets.collect())
+            .map(CollectedTargetsStrs::from_vec)
     }
 }
 
@@ -20,12 +27,41 @@ impl<'a> Iterator for TargetsIter<'a> {
     }
 }
 
+pub struct CollectedTargetsStrs<'a>(Vec<&'a str>);
+
+impl<'a> CollectedTargetsStrs<'a> {
+    fn from_vec(vec: Vec<&'a str>) -> Self {
+        #[cfg(debug_assertions)]
+        for window in vec.windows(2) {
+            assert!(
+                window[0] < window[1],
+                "The targets from `go tool dist list` were expected to be sorted lexicographically."
+            );
+        }
+        Self(vec)
+    }
+}
+
+impl<'a> CollectedTargetsStrs<'a> {
+    pub fn contains(&self, target: &str) -> bool {
+        self.0.binary_search(&target).is_ok()
+    }
+}
+
+impl<'a> Deref for CollectedTargetsStrs<'a> {
+    type Target = [&'a str];
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_slice()
+    }
+}
+
 /// Returns the list of targets supported by the current go compiler using CLI.
-/// 
+///
 /// The list of targets can be iterated over using the `iter` method:
-/// 
+///
 /// # Example
-/// 
+///
 /// ```rust
 #[doc = include_str!("../examples/targets.rs")]
 /// ```
@@ -35,6 +71,6 @@ pub fn from_cli() -> Result<Targets, std::io::Error> {
         .arg("dist")
         .arg("list")
         .output()?;
-    
+
     Ok(Targets(output.stdout))
 }
